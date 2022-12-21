e2b53c7a729051b01e6e175087a9fe8b5ab351c2
romain.callewaert <callewaertromain@gmail.com>
2022-12-21 15:21:36 +0100

    [add]song

--- ClassGestionnaireCommandes.py
+++ ClassGestionnaireCommandes.py
@@ -3,7 +3,7 @@
 class GestionnaireCommandes:
       
      
-    def execCommand(self, commandName, commandParametres):
+    async def execCommand(self, commandName, commandParametres, message, client):
                 
         if(commandName == "ping"):
             from commandes.ping import ping
@@ -20,4 +20,22 @@ class GestionnaireCommandes:
         elif(commandName == "anime"):
             from commandes.anime import anime
             return anime(commandParametres)
+        elif(commandName == "join"):
+            from commandes.song import join
+            return await join(message)
+        elif(commandName == "leave"):
+            from commandes.song import leave
+            return await leave(message, client)
+        elif(commandName == "play"):
+            from commandes.song import play
+            return await play(message, commandParametres)
+        elif(commandName == "pause"):
+            from commandes.song import pause
+            return await pause(message, client)
+        elif(commandName == "resume"):
+            from commandes.song import resume
+            return await resume(message, client)
+        elif(commandName == "stop"):
+            from commandes.song import stop
+            return await stop(message, client)
         return
